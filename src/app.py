95b19269785494e8c0395900de4e13297c83061b
zyc00 <1185279688@qq.com>
2024-04-02 16:20:15 +0000

    ply for pointcloud

--- src/app.py
+++ src/app.py
@@ -1,10 +1,38 @@
 import argparse
 import os
+import numpy as np
 
 from flask import Flask, jsonify, request, send_from_directory
 
 app = Flask(__name__, static_folder="static")
 
+def load_ply(filename):
+    with open(filename, "r") as rf:
+        while True:
+            try:
+                line = rf.readline()
+            except:
+                raise NotImplementedError
+            if "end_header" in line:
+                break
+            if "element vertex" in line:
+                arr = line.split()
+                num_of_points = int(arr[2])
+
+        # print("%d points in ply file" %num_of_points)
+        points = np.zeros([num_of_points, 6])
+        for i in range(points.shape[0]):
+            point = rf.readline().split()
+            assert len(point) == 6
+            points[i][0] = float(point[0])
+            points[i][1] = float(point[1])
+            points[i][2] = float(point[2])
+            points[i][3] = float(point[3])
+            points[i][4] = float(point[4])
+            points[i][5] = float(point[5])
+    rf.close()
+    del rf
+    return points
 
 def list_dir(path, add_parent=False):
     """List directory contents, distinguish between files and directories."""
@@ -46,6 +74,25 @@ def files():
 def serve_file(path):
     """Serve a file from the static directory."""
     return app.send_static_file(path)
+
+@app.route("/pointcloud/<path:path>")
+def pointcloud_server(path):
+    global obj_path
+    obj_path = path
+    points = load_ply(f"{path}")
+    xyz = points[:, :3]
+    rgb = points[:, 3:6] / 255
+
+    # normalize
+    shift = xyz.mean(0)
+    scale = np.linalg.norm(xyz - shift, axis=-1).max()
+    xyz = (xyz - shift) / scale
+
+    # flatten
+    xyz = xyz.flatten()
+    rgb = rgb.flatten()
+
+    return jsonify({"xyz": xyz.tolist(), "rgb": rgb.tolist()})
 
 
 @app.route("/file/<path:path>")
--- src/static/script.js
+++ src/static/script.js
@@ -1,4 +1,80 @@
+import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import { TrackballControls } from "three/addons/controls/TrackballControls.js";
+
 var viewer = null;
+var geometry, points, scene;
+var geometry_origin, points_origin, scene_origin;
+
+function createLights() {
+    const ambientLight = new THREE.AmbientLight("white", 2);
+  
+    const mainLight = new THREE.DirectionalLight("white", 5);
+    mainLight.position.set(10, 10, 10);
+  
+    return { ambientLight, mainLight };
+}
+
+async function loadPointCloud(ply_path) {
+    console.log("Loading point cloud:", ply_path);
+    var response = await fetch(ply_path);
+    var data = await response.json();
+    var positions = data.xyz;
+    var colors = data.rgb;
+    var material = new THREE.PointsMaterial({
+      size: 10,
+      vertexColors: true,
+    });
+  
+    geometry = new THREE.BufferGeometry();
+    geometry.setAttribute(
+      "position",
+      new THREE.Float32BufferAttribute(positions, 3)
+    );
+    geometry.setAttribute("color", new THREE.Float32BufferAttribute(colors, 3));
+  
+    points = new THREE.Points(geometry, material);
+    scene.add(points);
+}
+
+async function loadPointCloudOrigin(ply_path) {
+    console.log("Loading point cloud:", ply_path);
+    var response = await fetch(ply_path);
+    var data = await response.json();
+    var positions = data.xyz;
+    var colors = data.rgb;
+    var material = new THREE.PointsMaterial({
+      size: 10,
+      vertexColors: true,
+    });
+  
+    geometry_origin = new THREE.BufferGeometry();
+    geometry_origin.setAttribute(
+      "position",
+      new THREE.Float32BufferAttribute(positions, 3)
+    );
+    geometry_origin.setAttribute("color", new THREE.Float32BufferAttribute(colors, 3));
+  
+    points_origin = new THREE.Points(geometry_origin, material);
+    scene_origin.add(points_origin);
+}
+
+function syncControls(controls, controls_origin) {
+    // Sync controls
+    controls.addEventListener('change', () => {
+        controls_origin.target.copy(controls.target);
+        controls_origin.object.position.copy(controls.object.position);
+        controls_origin.object.zoom = controls.object.zoom;
+        controls_origin.object.quaternion.copy(controls.object.quaternion);
+    });
+
+    controls_origin.addEventListener('change', () => {
+        controls.target.copy(controls_origin.target);
+        controls.object.position.copy(controls_origin.object.position);
+        controls.object.zoom = controls_origin.object.zoom;
+        controls.object.quaternion.copy(controls_origin.object.quaternion);
+    });
+}
 
 function fetchDirectory(path) {
     fetch(`/files?path=${path}`)
@@ -14,7 +90,79 @@ function fetchDirectory(path) {
                         // If item is a directory, fetch its contents
                         fetchDirectory(`${path}/${item}`);
                     } else {
-                        if (!['.obj', '.stl', '.glb', '.ply'].some(ext => item.endsWith(ext))) {
+                        if (['.ply'].some(ext => item.endsWith(ext))) {
+                            console.log('Loading point cloud:', item);
+                            scene = new THREE.Scene();
+
+                            // Initialize variables
+                            var canvas = document.getElementById("viewer");
+
+                            var canvas_width = canvas.getBoundingClientRect().width;
+                            var canvas_height = canvas.getBoundingClientRect().height;
+
+                            // Initialize camera
+                            var camera = new THREE.OrthographicCamera(
+                                canvas_width / -800, // Left
+                                canvas_width / 800, // Right
+                                canvas_height / 800, // Top
+                                canvas_height / -800, // Bottom
+                                1, // Near
+                                1000 // Far
+                            );
+                            camera.position.z = 5;
+
+                            // Initialize renderer
+                            var renderer = new THREE.WebGLRenderer({
+                                canvas: canvas,
+                                antialias: true,
+                            });
+                            renderer.setSize(canvas_width, canvas_height);
+                            
+                            // Initialize controls
+                            var controls = new TrackballControls(camera, renderer.domElement);
+                            controls.enableDamping = true;
+                            controls.dampingFactor = 0.25;
+                            controls.enableZoom = true;
+
+                            // Load point cloud
+                            loadPointCloud(`/pointcloud/${path}/${item}`).then(() => {
+                                function animate() {
+                                    requestAnimationFrame(animate);
+                                    renderer.render(scene, camera);
+                                    controls.update();
+                                }
+                                animate();
+                            });
+
+
+                            // load fused point cloud
+                            // console.log('Loading point cloud:', item);
+                            // scene_origin = new THREE.Scene();
+
+                            // // Initialize variables
+                            // var canvas_origin = document.getElementById("origin-viewer");
+                            // var canvas_width_origin = canvas_origin.getBoundingClientRect().width;
+                            // var canvas_height_origin = canvas_origin.getBoundingClientRect().height;
+
+                            // // Initialize renderer
+                            // var renderer_origin = new THREE.WebGLRenderer({
+                            //     canvas: canvas_origin,
+                            //     antialias: true,
+                            // });
+                            // renderer_origin.setSize(canvas_width_origin, canvas_height_origin);
+
+                            // // Load point cloud
+                            // console.log(`/pointcloud/${path}/${item}`)
+                            // path.replace("pseudo_masks", "fused_rgb.ply")
+                            // loadPointCloudOrigin(`/pointcloud/${path.replace("pseudo_masks", "fused_rgb.ply")}`).then(() => {
+                            //     function animate_origin() {
+                            //         requestAnimationFrame(animate_origin);
+                            //         renderer_origin.render(scene_origin, camera);
+                            //         controls_origin.update();
+                            //     }
+                            //     animate_origin();
+                            // });
+                        } else if (!['.obj', '.stl', '.glb'].some(ext => item.endsWith(ext))) {
                             // If item is a file, display a simple preview
                             document.getElementById('preview').textContent = `Selected file: ${item}`;
                         } else {
